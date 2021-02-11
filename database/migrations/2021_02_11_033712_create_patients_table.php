af4ba5f81d5f165520c09fae3f884ac3b54c9435
Alberto Lujan <alujan2@seidor.es>
2021-02-10 23:24:01 -0600

    Modulo de consulting I - migraciones, controladores y modelos

---
+++ database/migrations/2021_02_11_033712_create_patients_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePatientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+
+        Schema::create('patients', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('dni', 15)->unique();
+            $table->string('firstname', 100)->nullable();
+            $table->string('lastname', 100)->nullable();
+            $table->date('birth');
+            $table->string('sex');
+            $table->string('address')->nullable();
+            $table->string('phone', 20)->nullable();
+            $table->string('email', 50)->nullable();
+            $table->string('apoderado', 200)->nullable();
+            $table->integer('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('patients');
+    }
+}
